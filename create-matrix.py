29f53fa1f6a960835cf05d2b3c6df09c06893384
Guillaume Renard <grenard@lucca.fr>
2023-11-17 18:29:47 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -4,7 +4,6 @@ matrix = {
     "include": [ 
         {"cluster": "cluster-1",},
         {"cluster": "cluster-2",},
-        {"cluster": "cluster-3",},
     ],
 }
-os.environ["GITHUB_OUTPUT"] = "matrix=" + json.dumps(matrix).replace('"', '\\"').replace(" ", "")
+print("matrix=" + json.dumps(matrix).replace('"', '\\"').replace(" ", ""))
