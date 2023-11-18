e245b0870cf1d483a8569afc444aa6a630f7d942
Guillaume Renard <grenard@lucca.fr>
2023-11-18 14:38:07 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -4,6 +4,22 @@ from types import SimpleNamespace
 from github import Github
 from github import Auth
 
+# convert json string from env variable GITHUB_CONTEXT to object
+github_context = json.loads(os.environ.get('GITHUB_CONTEXT'), object_hook=lambda d: SimpleNamespace(**d))
+
+# GetEnvironmentString("GITHUB_EVENT_NAME") == "pull_request"
+if github_context.event.name == "pull_request":
+    # using an access token
+    auth = Auth.Token(github_context.token)
+    # Public Web Github
+    github_api = Github(auth=auth)
+    repo = github_api.get_repo(github_context.repository)
+    pr = repo.get_pull(1)
+    print("pr files:")
+    for file in pr.get_files():
+        print(file)
+
+
 clusters_matrix = {
     "include": [ 
         {"ClusterName": "cluster-1", "ManifestPath": "cluster-1.yaml",},
@@ -11,24 +27,7 @@ clusters_matrix = {
         {"ClusterName": "cluster-3", "ManifestPath": "cluster-3.yaml",},
     ],
 }
-
-# clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
-# with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
-#     f.write("max-parallel=3" + "\n")
-#     f.write("clusters-matrix=" + clustersMatrixString)
-
-# convert json string from env variable GITHUB_CONTEXT to object
-github = json.loads(os.environ.get('GITHUB_CONTEXT'), object_hook=lambda d: SimpleNamespace(**d))
-# print("diff_url:")
-print(github.event)
-
-
-# using an access token
-auth = Auth.Token(github.token)
-# Public Web Github
-g = Github(auth=auth)
-repo = g.get_repo(github.repository)
-pr = repo.get_pull(1)
-print("pr files:")
-for file in pr.get_files():
-    print(file.filename)
+clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
+with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
+    f.write("max-parallel=3" + "\n")
+    f.write("clusters-matrix=" + clustersMatrixString)
