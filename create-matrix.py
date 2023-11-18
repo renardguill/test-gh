f4c421f79b79eb8c44d9bb405c6a52966c8238bc
Guillaume Renard <grenard@lucca.fr>
2023-11-18 12:30:50 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -1,6 +1,9 @@
 import os
 import json
 from types import SimpleNamespace
+from github import Github
+from github import Auth
+from github import PullRequest
 
 clusters_matrix = {
     "include": [ 
@@ -10,17 +13,38 @@ clusters_matrix = {
     ],
 }
 
-clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
-with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
-    f.write("max-parallel=3" + "\n")
-    f.write("clusters-matrix=" + clustersMatrixString)
+# clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
+# with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
+#     f.write("max-parallel=3" + "\n")
+#     f.write("clusters-matrix=" + clustersMatrixString)
+
+
+# get string content of file context.json
+with open("context.json") as f:
+    context = f.read()
+
+# convert json string from file context.json to object
+context = json.loads(context, object_hook=lambda d: SimpleNamespace(**d))
+print("context:")
+print(context)
 
 
 # convert json string from env variable GITHUB_CONTEXT to object
 github = json.loads(os.environ.get('GITHUB_CONTEXT'), object_hook=lambda d: SimpleNamespace(**d))
 print("diff_url:")
-print(github)
-print(github.diff_url)
+print(github.event.pull_request.diff_url)
+
+
+# using an access token
+auth = Auth.Token(os.environ.get('GITHUB_TOKEN'))
+# Public Web Github
+g = Github(auth=auth)
+repo = g.get_repo(github.repository)
+pr = repo.get_pull(github.event.number)
+print("pr files:")
+for file in pr.get_files():
+    print(file.filename)
+
 
 print("GITHUB_ENV:")
 # print content of file in env variable GITHUB_ENV
