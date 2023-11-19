1b169cb320b1f872501a8bbb8ee667cb069d0fbf
Guillaume Renard <grenard@lucca.fr>
2023-11-19 13:05:48 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -1,8 +1,14 @@
 import os
 import json
+import glob
 from types import SimpleNamespace
 from github import Github
 from github import Auth
+
+
+files = glob.glob("clusters/**/*.yaml", recursive=True)
+for file in files:
+    print(file)
 
 # convert json string from env variable GITHUB_CONTEXT to object
 github_context = json.loads(os.environ.get('GITHUB_CONTEXT'), object_hook=lambda d: SimpleNamespace(**d))
@@ -21,14 +27,15 @@ if github_context.event_name == "pull_request":
     print("pr files:")
     for file in pr.get_files():
         if file.filename.startswith("clusters/") and file.filename.endswith(".yaml"):
+            file_name = file.filename
+            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file_name}]
             print(file)
-            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file.filename.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file.filename}]
 else:
     print("not a pull request")
-    for filename in os.listdir('clusters'):
-        if filename.endswith(".yaml"):
-            print(filename)
-            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": filename.replace(".yaml", ""), "ManifestPath": "clusters/" + filename}]
+    for file_name in glob.glob("clusters/**/*.yaml", recursive=True):
+        print(file_name)
+        clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file_name}]
+
     print(github_context)
 
 clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
