a751d46164ec6ded94423c134de9c2f233505b35
Guillaume Renard <grenard@lucca.fr>
2023-11-19 17:35:14 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -4,12 +4,12 @@ import glob
 from types import SimpleNamespace
 from github import Github
 from github import Auth
+from git import Repo
 
-
-files = glob.glob("clusters/**/*.yaml", recursive=True)
-for file in files:
-    print(file)
-
+git_repo = Repo(".")
+commit = git_repo.commit("main")
+for item in git_repo.index.diff(None):
+    print(item.a_path)
 # convert json string from env variable GITHUB_CONTEXT to object
 github_context = json.loads(os.environ.get('GITHUB_CONTEXT'), object_hook=lambda d: SimpleNamespace(**d))
 
@@ -22,8 +22,8 @@ if github_context.event_name == "pull_request":
     auth = Auth.Token(github_context.token)
     # Public Web Github
     github_api = Github(auth=auth)
-    repo = github_api.get_repo(github_context.repository)
-    pr = repo.get_pull(1)
+    github_repo = github_api.get_repo(github_context.repository)
+    pr = github_repo.get_pull(1)
     print("pr files:")
     for file in pr.get_files():
         if file.filename.startswith("clusters/") and file.filename.endswith(".yaml"):
@@ -35,8 +35,10 @@ else:
     for file_name in glob.glob("clusters/**/*.yaml", recursive=True):
         print(file_name)
         clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file_name}]
-
-    print(github_context)
+    git_repo = Repo(github_context.workspace)
+    commit = git_repo.commit(github_context.ref_name)
+    for item in git_repo.index.diff(None):
+        print(item.a_path)
 
 clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
 with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
