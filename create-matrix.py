9c158605cd9eb0e932e6e70de0fe97c28ed956cf
Guillaume Renard <grenard@lucca.fr>
2023-11-20 10:15:32 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -28,15 +28,14 @@ if github_context.event_name == "pull_request":
             file_name = file.filename
             clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id, "ManifestPath": file_name + " in base ref: " + github_context.base_ref, "ChangeType": "Create"}]
             clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id, "ManifestPath": file_name + " in head ref: " + github_context.head_ref, "ChangeType": "Update"}]
-            print(file)
+            print(file.status + " " + file.filename)
     print("commit files:")
-    git_repo = Repo(github_context.workspace)
-    base_commit = git_repo.commit("origin/" + github_context.base_ref)
-    for item in base_commit.diff(None):
-        print(item)
-        if item.a_path.startswith("clusters/") and item.a_path.endswith(".yaml"):
-            file_name = item.a_path
-            print(file_name)
+    commit = github_repo.get_commit(sha=github_context.sha)
+    for file in commit.files:
+        if file.filename.startswith("clusters/") and file.filename.endswith(".yaml"):
+            file_name = file.filename
+            # clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id, "ManifestPath": file_name, "ChangeType": "CreateOrUpdate"}]
+            print(file.status + " " + file.filename)
 else:
     # print("not a pull request")
     # for file_name in glob.glob("clusters/**/*.yaml", recursive=True):
