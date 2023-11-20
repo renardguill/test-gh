33ac32c9d3843ba9c551cc3d1a041a846381a8f8
Guillaume Renard <grenard@lucca.fr>
2023-11-20 09:24:31 +0100

    a

--- create-matrix.py
+++ create-matrix.py
@@ -26,8 +26,8 @@ if github_context.event_name == "pull_request":
     for file in pr.get_files():
         if file.filename.startswith("clusters/") and file.filename.endswith(".yaml"):
             file_name = file.filename
-            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id + "-1", "ManifestPath": file_name + "in base ref: " + github_context.base_ref}]
-            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id + "-2", "ManifestPath": file_name + "in head ref: " + github_context.head_ref}]
+            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id, "ManifestPath": file_name + " in base ref: " + github_context.base_ref, "ChangeType": "Create"}]
+            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", "-") + github_context.run_id, "ManifestPath": file_name + " in head ref: " + github_context.head_ref, "ChangeType": "Update"}]
             print(file)
     print("commit files:")
     git_repo = Repo(github_context.workspace)
@@ -49,7 +49,7 @@ else:
         if item.a_path.startswith("clusters/") and item.a_path.endswith(".yaml"):
             file_name = item.a_path
             print(file_name)
-            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file_name}]
+            clusters_matrix['include'] = clusters_matrix.get('include', []) + [{"ClusterName": file_name.replace("clusters/", "").replace("/", "-").replace(".yaml", ""), "ManifestPath": file_name, "ChangeType": "CreateOrUpdate"}]
 
 clustersMatrixString = json.dumps(clusters_matrix).strip().replace(" ", "")
 with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
