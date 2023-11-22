8c815cd0a1a7135c8c9b4c9c049767875d332bd4
Guillaume Renard <grenard@lucca.fr>
2023-11-22 01:24:47 +0100

    a

--- .gitops/cleanup-environments.py
+++ .gitops/cleanup-environments.py
@@ -13,7 +13,9 @@ github_repo = github_api.get_repo(os.environ.get('GITHUB_REPOSITORY'))
 #         github_repo.delete_environment(environment.id)
 
 clusters_matrix = json.loads(os.environ.get('CLUSTERS_MATRIX'), object_hook=lambda d: SimpleNamespace(**d))
-for cluster in clusters_matrix.get("include", []):
+print("clusters_matrix:")
+print(clusters_matrix)
+for cluster in clusters_matrix.include:
     if cluster.ClusterName.endswith("_tmp"):
         environmentId = github_repo.get_environment(cluster.ClusterName).id
         github_repo.delete_environment(environmentId)
